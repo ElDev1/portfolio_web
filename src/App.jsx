71ef9f02d006a97d5cfd5f588db083549d918ec0
Devi Amaolo <deviamaolo@gmail.com>
2022-03-18 02:05:46 -0300

    creating components and general css

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import React from 'react'
+import Nav from './components/nav/Nav';
+import Header from './components/header/Header';
+import Portfolio from './components/portfolio/Portfolio';
+import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 export const App = () => {
   return (
-    <div>app</div>
+    <div>
+      <Header />
+      <Nav />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
   )
 }
 
