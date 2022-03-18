b36e02e35462ae7796872493927a11701cf6996d
Devi Amaolo <deviamaolo@gmail.com>
2022-03-18 16:32:41 -0300

    creating header and components

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,25 @@
 import React from 'react';
 import './header.css';
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
 
 export const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Devi Amaolo</h1>
+        <h5 className="text-light">Frontend Developer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="me" />
+        </div>
+
+        <a href="#contact" className="scroll_down">Scroll Down</a>
+
+      </div>
+    </header>
   )
 }
 
