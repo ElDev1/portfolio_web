f341206869e52cb95276e9b9138fbbc90252a0d9
Devi Amaolo <deviamaolo@gmail.com>
2022-03-19 01:55:57 -0300

    creating nav and css styles with hooks

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,9 +1,20 @@
 import React from 'react';
 import './nav.css';
+import {AiOutlineHome} from 'react-icons/ai'
+import {BiMessageSquareDetail} from 'react-icons/bi'
+import {MdWorkOutline} from 'react-icons/md'
+import { useState } from 'react';
 
-export const Nav = () => {
+
+const Nav = () => {
+  const [activeNav, setActiveNav] = useState('#');
+
   return (
-    <div>Nav</div>
+    <nav>
+      <a href="#" onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
+      <a href="#portfolio" onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><MdWorkOutline/></a>
+      <a href="#contact" onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageSquareDetail/></a>
+    </nav>
   )
 }
 
