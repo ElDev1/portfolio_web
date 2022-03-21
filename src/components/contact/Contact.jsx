5c91c0172067d10433efe25e5373c5021abe7b11
Devi Amaolo <deviamaolo@gmail.com>
2022-03-21 15:13:16 -0300

    configuring form

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -2,8 +2,25 @@ import React from 'react'
 import './contact.css'
 import {MdOutlineEmail} from 'react-icons/md'
 import {AiOutlineLinkedin} from 'react-icons/ai'
+import { useRef } from 'react';
+import emailjs from 'emailjs-com';
 
 export const Contact = () => {
+    const form = useRef();
+  
+    const sendEmail = (e) => {
+      e.preventDefault();
+  
+      emailjs.sendForm('service_9xxbjwr', 'template_7yfar38', form.current, 'AnQKcm16043-jLyjV')
+        .then((result) => {
+            console.log(result.text);
+        }, (error) => {
+            console.log(error.text);
+        });
+
+      e.target.reset();
+    };
+
   return (
     <section id='contact'>
       <h5>Get In Touch</h5>
@@ -24,7 +41,7 @@ export const Contact = () => {
             <a href="https://www.linkedin.com/in/devi-amaolo-773135161/" target="_blank">Contact me</a>
           </article>
         </div>
-          <form action="">
+          <form ref={form} onSubmit={sendEmail}>
             <input type="text" name='name' placeholder='Your Full Name' required />
             <input type="text" name='email' placeholder='Your Email' required/>
             <textarea name="message" rows="7" placeholder="Your Message" required></textarea>
